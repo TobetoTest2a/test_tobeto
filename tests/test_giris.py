e6229b08ddd7b3c47d16f091cb5a74c522430736
eliftabanca <elifbusratabanca@gmail.com>
2024-04-18 20:11:57 +0300

    Sonuç raporunu HTML de göstermek için ekleme yapıldı.

--- tests/test_giris.py
+++ tests/test_giris.py
@@ -8,5 +8,6 @@ class TestTobetoGiris(softest.TestCase):
     
     def test_validUserName_validPassword_girisYap(self):
         giris_instance = Giris(self.driver) #baska sayfadan cagırılan fonksiyon
-        assert giris_instance.gecerli_giris() ==  BASARILI_POPUP_TEXT
-        
+        self.soft_assert(self.assertEqual, BASARILI_POPUP_TEXT, giris_instance.gecerli_giris, "Hatali basarili mesaji.")
+        #assert giris_instance.gecerli_giris() ==  BASARILI_POPUP_TEXT
+        #self.assert_all()
