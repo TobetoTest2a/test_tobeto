5ae366688f5037aa7cbd27b42171f2b6f65d50a6
Zehrasenturk1828 <Zehrasenturk778@gmail.com>
2024-04-20 17:28:34 +0300

    update yabanci_dillerim and added medyaHesaplarim

--- tests/test_yabanci_dillerim.py
+++ tests/test_yabanci_dillerim.py
@@ -10,10 +10,9 @@ class TestTobetoGiris(softest.TestCase,PageBase):
     
     def test_yabanci_dil_ekle(self):
         giris = Giris(self.driver)
-        giris.ana_giris()
-
         yabanci_dillerim = Yabanci_dil_ekle(self.driver) #baska sayfadan cagırılan fonksiyon
-
+        
+        giris.ana_giris()
         yabanci_dillerim.profilim_kismina_tikla()
         yabanci_dillerim.profil_bilgileri_tikla()
         yabanci_dillerim.yabanci_dillerim_tikla()
@@ -22,16 +21,16 @@ class TestTobetoGiris(softest.TestCase,PageBase):
         yabanci_dillerim.seviye_acilirmenu_tikla()
         yabanci_dillerim.seviye_sec()
         yabanci_dillerim.kaydet()
-        self.soft_assert(self.assertEqual, BASARILI_POPUP_TEXT, yabanci_dillerim.kaydetme_basarili(), "HATALI BASARILI MESAJI")
+        self.soft_assert(self.assertEqual, POPUP_MESSAGE_TEXT, yabanci_dillerim.kaydetme_basarili(), "HATALI BASARILI MESAJI")
+        self.assert_all()
         
 
     
     def test_yabanci_dil_bos_alan_kontrolu(self):
         giris = Giris(self.driver)
-        giris.ana_giris()
-
         yabanci_dillerim = Yabanci_dil_bos_gecme(self.driver)
 
+        giris.ana_giris()
         yabanci_dillerim.profilim_kismina_tikla()
         yabanci_dillerim.profil_bilgileri_tikla()
         yabanci_dillerim.yabanci_dillerim_tikla()
@@ -39,7 +38,22 @@ class TestTobetoGiris(softest.TestCase,PageBase):
         yabanci_dillerim.yabancidil_sec()
         yabanci_dillerim.seviye_acilirmenu_tikla()
         yabanci_dillerim.kaydet()
-        yabanci_dillerim.doldurulmasi_zorunlu_alan_mesaji()
+        self.soft_assert(self.assertEqual, BLANK_MESSAGE_TEXT, yabanci_dillerim.doldurulmasi_zorunlu_alan_mesaji(), "HATALI MESAJ")
+        self.assert_all()
+
+    
+    def test_yabanci_dil_silme_kontrolu(self):
+        giris = Giris(self.driver)
+        yabanci_dillerim = Yabanci_dil_silme(self.driver)
+
+        giris.ana_giris()
+        yabanci_dillerim.profilim_kismina_tikla()
+        yabanci_dillerim.profil_bilgileri_tikla()
+        yabanci_dillerim.yabanci_dillerim_tikla()
+        yabanci_dillerim.sil_butonuna_tikla()
+        yabanci_dillerim.silmeyi_onayla()
+        self.soft_assert(self.assertEqual, DELETE_POPUP_MESSAGE, yabanci_dillerim.silme_islemi_basarili(), "HATALI MESAJ")
+        self.assert_all()
 
 
         
