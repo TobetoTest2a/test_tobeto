0f73e779543db930fc3d90755423823a7d0d14e3
Mehmet <mehmeturgutaksel@gmail.com>
2024-04-22 11:35:33 +0300

    profil fotografı yukleme testi ve tc kimlik testi yuklendi

--- pages/kisisel_bilgilerim.py
+++ pages/kisisel_bilgilerim.py
@@ -6,6 +6,7 @@ from pages.PageBase import PageBase
 from pages.constants.kisisel_bilgilerim_constants import *
 from pages.giris import Giris
 from time import sleep
+import re
 
 @pytest.mark.usefixtures("setup")
 class kisisel_bilgiler(PageBase):
@@ -42,4 +43,81 @@ class kisisel_bilgiler(PageBase):
     def sol_menude_goruntule(self):
         expected_sol_menu = ["Kişisel Bilgilerim", "Deneyimlerim", "Eğitim Hayatım", "Yetkinliklerim", "Sertifikalarım", "Medya Hesaplarım", "Yabancı Dillerim", "Ayarlar"]
         return expected_sol_menu 
+    
+    def ulke_bolumunde_yildiz_goruntulen(self):
+        ulke_bolumu = "Ülke*"
+        return ulke_bolumu
+    
+    def foto_cek(self):
+        screenshot_path = SCREENSHOT_FOLDER
+        self.driver.save_screenshot(screenshot_path)
+
+    def bayrak_butonuna_tikla(self):
+        self.wait_element_visibility(BAYRAK_BUTON_LOCATED).click()
+    
+    def bayrak_sec(self):
+        self.wait_element_visibility(BAYRAK_SEC_LOCATED).click()
+    
+    def telefon_no_gir(self):
+        telefon_no = self.wait_element_visibility(TELEFON_NO_LOCATED)
+        telefon_no.send_keys(TELEFON_NO_DATA)
+    
+    def kayitli_isim_dogrulama(self):
+        isim = self.wait_element_visibility(KSB_AD_LOCATE)
+        isim_value = isim.get_attribute("value")
+        return isim_value
+    
+    def kayitli_soyadi_dogrulama(self):
+        soyadi = self.wait_element_visibility(KSB_SOYAD_LOCATE)
+        soyadi_value = soyadi.get_attribute("value")
+        return soyadi_value
+    
+    def kayitli_mail_dogrulama(self):
+        mail = self.wait_element_visibility(KSB_MAIL_LOCATE)
+        self.driver.execute_script("arguments[0].removeAttribute('disabled');", mail)
+        mail_value = mail.get_attribute("value")
+        return mail_value
+    
+    def kayitli_tel_dogrulama(self):
+        tel = self.wait_element_visibility(KSB_TEL_NO_LOCATE)
+        tel_value = tel.get_attribute("value")
+        return tel_value
+    
+    def il_ilce_liste_kontrolu(self):
+        il_dropdown_menu = self.wait_element_visibility(IL_MENU_LOCATE)
+        il_dropdown_menu.click()
+        ilce_sec = self.wait_element_visibility(IL_ISTANBUL_PATH)
+        ilce_sec.click()
+        ilce_dropdown_menu = self.wait_element_visibility(ILCE_MENU_LOCATE)
+        ilce_dropdown_menu.click()
+        ilce_sec = self.wait_element_visibility(ILCE_BEYLIKDUZU_PATH)
+        ilce_sec.click()
+
+#####################
+     
+
+    def tc_kimlik_no_gir(self):
+        tc_kimlik = self.wait_element_visibility(TC_KIMLIK_LOCATED)
+        tc_kimlik.send_keys(GECERSIZ_TC_KIMLIK_DATA)
+
+#####################
+
+    def dosya_duzenleme_butonuna_tikla(self):
+        dosya_duzenleme_butonu = self.wait_element_visibility(PP_DUZENLEME_BUTON_LOCATE)
+        dosya_duzenleme_butonu.click()
+    
+    def pop_up_metinini_goruntule(self):
+        pop_up_metini = self.wait_element_visibility(DOSYA_YUKLEME_ILK_LOCATE)
+        return pop_up_metini.text
+    
+    def gozat_butonunu_ve_mesaji_goruntule(self):
+        gozat_butonu_mesaj = self.wait_element_visibility(GOZAT_LOCATE)
+        return gozat_butonu_mesaj.text
+    
+    def resim_yukle(self):
+        gozat_buton = self.wait_element_visibility(GOZAT_BUTON_LOCATE).send_keys(DOSYA1_PATH)
+        submit_buton = self.wait_element_visibility(DOSYA_SUBMIT_LOCATE)
+        submit_buton.click()
+    
+
         
