2556f845eed9626c206b927fbe59c90d7280786a
Oguz4242 <158754256+Oguz4242@users.noreply.github.com>
2024-04-22 12:26:21 +0300

    Güncelleme

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 import pytest
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service as ChromeService
 from pages.constants.globalConstants import *
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome()
     driver.get(BASE_URL)
     driver.maximize_window()
     request.cls.driver = driver
