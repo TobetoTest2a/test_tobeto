87211602da3d4acff5ae8f735f6c0007994150c9
gonca-zeliha <goncakoc92@hotmail.com>
2024-04-30 23:15:30 +0300

    APİ test egitim hayatım

--- tests_API/test_API_egitim_hayatim.py
+++ tests_API/test_API_egitim_hayatim.py
@@ -1,7 +1,8 @@
 
 import requests
+import pytest
 
-class TobetoAPI:
+class TestTobetoAPI():
     def __init__(self, token):
         self.base_url = "https://api.tobeto.com/api"
         self.token = token
@@ -9,28 +10,29 @@ class TobetoAPI:
             "Authorization": f"Bearer {token}"
         }
 
-    def get_egitimler(self):
+    def test_get_egitimler(self):
         url = f"{self.base_url}/educations"
         cevap = requests.get(url, headers=self.headers)
         return cevap.json() if cevap.status_code == 200 else None
 
-def main():
+    def test_main():
     # API token
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjkwNzcsImlhdCI6MTcxNDQ4NjI2OCwiZXhwIjoxNzE0NjU5MDY4fQ.5dxCDDcZv5TvsQYlwGf6KUVHvItJJuyKoRdPsp05gNY"
-    
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjkwNzcsImlhdCI6MTcxNDUwNzU3NiwiZXhwIjoxNzE0NjgwMzc2fQ.YD88QvjT7Gai-OoqwV4Nqc0vLWd4kfHOg3jsyVEB_9w"
+     
     # TobetoApı yi başlatıyorum.
-    api = TobetoAPI(token)
+        api = TestTobetoAPI(token)
     
     # eğitim verilerini aldım.
-    egitimler = api.get_egitimler()
+        egitimler = api.test_get_egitimler()
     
    
-    if egitimler:
-        print("Egitim verileri:")
-        print(egitimler)
-    else:
-        print("Eğitim verileri alınırken bir hata oluştu.")
+        if egitimler:
+            print("Egitim verileri:")
+            print(egitimler)
+        else:
+            print("Eğitim verileri alınırken bir hata oluştu.")
 
-if __name__ == "__main__":
-    main()
+        # if __name__ == "__main__":
+        
+TestTobetoAPI.test_main()
 
