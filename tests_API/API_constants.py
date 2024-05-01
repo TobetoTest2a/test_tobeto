809c132c524f11feaa5e8841aa79fcbce2ed4b65
Mehmet <mehmeturgutaksel@gmail.com>
2024-05-01 16:39:45 +0300

    kisisel bilgilerim api test ve api constantsları eklendi

--- tests_API/API_constants.py
+++ tests_API/API_constants.py
@@ -1,6 +1,6 @@
 base_url="https://api.tobeto.com/api"
 
-token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjkwNzcsImlhdCI6MTcxNDQ2MzQwNSwiZXhwIjoxNzE0NjM2MjA1fQ.2Dywvgm65jcJSiRPSc_EbmGKEzEaDwQsGOZjwV-Qrik"
+token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjkwNzcsImlhdCI6MTcxNDU2Njg0NSwiZXhwIjoxNzE0NzM5NjQ1fQ.Bt2Gsma8emgG0S_K4H4_eCeF9UpZPm5oZiRzVWuYppU"
 
 #Deneyimlerim
 expected_corporationName= "deneme"
@@ -9,3 +9,9 @@ expected_sector= "Finans"
 expected_country= "Ağrı"
 expected_StartDate= "2024-04-24"
 expected_EndDate=None
+
+#kişisel_bilgilerim
+expected_name = "Tobeto"
+expected_surname = "Tobeto"
+expected_mail = "tobeto.0001@gmail.com"
+expected_phoneNumber = "+905050000000" 
