55ea1736fea78a3410e77c022ee5384c1dd87cd2
Zehrasenturk1828 <Zehrasenturk778@gmail.com>
2024-05-01 16:47:59 +0300

    created test_api for social_media and foreign_language

---
+++ tests_API/API_constants.py
@@ -0,0 +1,6 @@
+url_socialMedia = "https://api.tobeto.com/api/user-social-medias"
+token_socialMedia = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjkwNzcsImlhdCI6MTcxNDM5Nzg5OSwiZXhwIjoxNzE0NTcwNjk5fQ.H-qA4wppIwWknEjfKE5d1apSZc3D7a8vfVpWPrOXFDE"
+
+url_language = "https://api.tobeto.com/api/user-foreign-languages"
+url_delete_language = "https://api.tobeto.com/api/user-foreign-languages/5436"
+token_language = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjkwNzcsImlhdCI6MTcxNDQ5MTkxOCwiZXhwIjoxNzE0NjY0NzE4fQ.JYacGh-W1pfQJaRQvWW6SUVkbdy9xDPTHnI37XDeEA0"
